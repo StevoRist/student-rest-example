7dca3239da984a5dfe098a54c92f3cf469f6ad20
Stevoo <stevche2121@gmail.com>
2022-03-24 11:56:09 +0100

    modify StudentController class

--- src/main/java/com/iwec/rest/example/controller/rest/StudentController.java
+++ src/main/java/com/iwec/rest/example/controller/rest/StudentController.java
@@ -20,11 +20,9 @@ import com.iwec.rest.example.model.Student;
 import com.iwec.rest.example.repository.StudentRepository;
 
 @RestController
- BE/EI-2/make-changes-to-the-same-file
-@RequestMapping("v2/rest/students")
 
 @RequestMapping("v2/rest/students")
- develop
+
 public class StudentController {
 
 	@Autowired
